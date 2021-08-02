fb1374a1ab383d483b4828a90c0f8a36d6b42c64
misiekhardcore <misiekhardcore@gmail.com>
2021-08-02 19:30:05 +0200

    Collect all entities in one file

---
+++ src/entities/index.ts
@@ -0,0 +1,9 @@
+import { UserAddress } from "./Address";
+
+import { Comment } from "./Comment";
+import { Order } from "./Order";
+import { OrderItem } from "./OrderItem";
+import { Product } from "./Product";
+import { Rating } from "./Rating";
+import { User } from "./User";
+export { User, Comment, Product, Rating, UserAddress as Address, Order, OrderItem };
